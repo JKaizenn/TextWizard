d464c90df48708351b204280378c2386aeba10a4
Jessen Forbush <jessenforbush@gmail.com>
2026-03-12 07:39:01 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/***********************
+ * PROGRAM SPECIFICATION AND DESCRIPTION:
+ * A user can create, edit, save, and open text files using the text editor, 
+ * a straightforward program. In addition to a user interface for the user to interact with, 
+ * it uses classes and objects to store and alter the content. 
+ * Basic text editing functions including 
+ * copying, pasting, deleting, searching, and printing text are all available in the text editor. 
+ * Text Editor is a simple application that is built into C++ programming language. 
+ * We can use Graphical User Interface (GUI) Libraries such as QT, and GTK+ for creating a graphical user interface. 
+ * Libraries such as Pango or Cairo are required to display text on the screen.
+ * 
+ * 
+ * C++ 23, CMAKE, OPENGL, GLFW
+*************************/
+
+
+
+int main()
+{
+    std::cout << "Hello Text Wiz!" << '\n';
+
+    return 0;
+}
