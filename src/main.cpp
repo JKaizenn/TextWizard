f0c2e033cb09408db8c86ab80d2330f36c9a3c6c
Jessen Forbush <jessenforbush@gmail.com>
2026-03-13 17:22:47 -0400

    finished main tutorial! Rectangle!!

--- src/main.cpp
+++ src/main.cpp
@@ -13,40 +13,45 @@
  * We can use Graphical User Interface (GUI) Libraries such as QT, and GTK+ for creating a graphical user interface. 
  * Libraries such as Pango or Cairo are required to display text on the screen.
  * 
- * 
  * C++ 23, CMAKE, OPENGL, GLFW
 *************************/
 
-
-
-
 int main() 
 {
-    // Initialize GLFW with glfwInit()
+    // Initialize GLFW
     if (!glfwInit())
     {
         // Handle Initialization failure
     }
 
-    // Set window hints (OpenGL version 4.1, Core profile)
+    // Set window hints
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    // Create a GLFWwindow* with glfwCreateWindow()
+    // Create window and context
     GLFWwindow* window = glfwCreateWindow(640, 480, "TextWizard", NULL, NULL);
-
-    // Make the window the current context with glfwMakeContextCurrent()
     glfwMakeContextCurrent(window);
 
-    // Initialize GLAD with gladLoadGLLoader()
+    // Initialize GLAD
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    // -------------------------------------------------------
-    // VERTEX SHADER
+    // Vertices and indices
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,   // top right
+        0.5f, -0.5f, 0.0f,   // bottom right
+       -0.5f, -0.5f, 0.0f,   // bottom left
+       -0.5f,  0.5f, 0.0f    // top left 
+    };
+    unsigned int indices[] = {
+        0, 1, 3,  // first triangle
+        1, 2, 3   // second triangle
+    };
 
-    // Store our vertex shader in a const C string
+    // -------------------------------------------------------
+    // SHADERS
+
     const char *vertexShaderSource = "#version 330 core\n"
         "layout(location = 0) in vec3 aPos;\n"
         "void main()\n"
@@ -54,74 +59,94 @@ int main()
         " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
         "}\0";
 
-    // Create Vertex Shader and assign it an ID
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char *fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\0";
 
-    /* Attach the shader source code to the shader object and compile the shader
-        Order of arguments: takes the shader object to compile to, 
-        Specifies how many strings we're passing as source code,
-        The actual source code of the vertex shader, NULL 
-    */
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-
-    // Check for successful compilation
     int success;
     char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 
-    if(!success)
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
-    // -------------------------------------------------------
-    // FRAGMENT SHADER
 
-
-
-
-
-
-
-    // -------------------------------------------------------
-    
-    // Triangle Vertices
-    float vertices[]
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
     {
-       -0.5f, -0.5f, -0.0f,
-        0.5f, -0.5f,  0.0f,
-        0.0f,  0.5f,  0.0f
-    };
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
-    // Assign a unique ID to the vertex buffer object
-    unsigned int VBO;
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // -------------------------------------------------------
+    // VAO -> VBO -> EBO -> Vertex Attributes
+
+    unsigned int VAO, VBO, EBO;
+
+    // 1. Generate and bind VAO first
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // 2. Generate, bind, and fill VBO
     glGenBuffers(1, &VBO);
-
-    // Assign our VBO to a buffer type (ARRAY_BUFFER)
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    /* Call glBufferData to copy our vertex data into the buffer's memory
-        Order of arguments: Type of Buffer, Specifies size of data in bytes,
-        actual data we want to send, specifies how we want the GPU to manage
-        the given data
-    */   
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // 3. Generate, bind, and fill EBO
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Main render loop - run while !glfwWindowShouldClose()
-    while(!glfwWindowShouldClose(window))
+    // 4. Set vertex attribute pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Unbind VAO
+    glBindVertexArray(0);
+
+    // -------------------------------------------------------
+    // Render loop
+
+    while (!glfwWindowShouldClose(window))
     {
-        glClearColor(2.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.53f, 0.812f, 0.941f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
-        
-    } 
+    }
 
-    // Cleanup - glfwDestroyWindow() and glfwTerminate()
+    // Cleanup
     glfwDestroyWindow(window);
     glfwTerminate();
 }
